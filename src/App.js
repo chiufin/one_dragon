31d08a7476ee4d90ecb72982bb2fc0b1e179cde0
stacy <chiufin@hotmail.com>
2022-06-11 10:53:47 +0100

    update init reactGA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { useEffect } from 'react';
 import logo from './logo.png';
 import './App.css';
 import ReactGA from 'react-ga';
 
-function App() {
-  ReactGA.initialize(process.env.REACT_APP_GA);
-  ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.initialize(process.env.REACT_APP_GA);
 
+function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
   return (
     <div className="App">
       <header className="App-header">
