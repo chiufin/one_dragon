538cb373e4f9247e410190bd10f6d0ff30323f3a
stacy <chiufin@hotmail.com>
2022-06-11 07:17:33 +0100

    install ga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.png';
 import './App.css';
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize(process.env.REACT_APP_GA);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <header className="App-header">
